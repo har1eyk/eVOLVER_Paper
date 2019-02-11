90534705bac7640802700ce44cf0f27299e14558
Harley <harleyk@gmail.com>
2019-02-11 07:57:21 -0500

    folder changes

--- Python Scripts/HS_libscreen/UDP_send.m
+++
@@ -1,9 +0,0 @@
-function recieved_data = UDP_send(config,ip,port)
-u = udp(ip,port,'timeout',3,'LocalPort',57379);
-fopen(u);
-flushinput(u);
-fwrite(u, config, 'char');
-recieved_data = fscanf(u);
-fclose(u);
-delete(u);
-clear u;
